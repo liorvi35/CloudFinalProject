1877d121347c7e729f03da67b5896c1d8b1e0f64
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2024-08-18 16:16:54 +0000

    cdk ts

---
+++ linkUp/test/link_up.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LinkUp from '../lib/link_up-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/link_up-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LinkUp.LinkUpStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
