45f87060744d5d0b4ea106c75c5456ef90bb423c
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2024-08-19 19:51:05 +0000

    try

--- linkup-cdk/lib/linkup-cdk-stack.ts
+++ linkup-cdk/lib/linkup-cdk-stack.ts
@@ -1,13 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class LinkupCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     
-    const labRole = iam.Role.fromRoleArn(this, 'Role', "arn:aws:iam::114985187174:role/LabRole", { mutable: false });
+    const labRole = iam.Role.fromRoleArn(this, 'Role', "arn:aws:iam::991888206011:role/LabRole", { mutable: false });
   
-    // create stack here using labRole
+    const storage = new s3.Bucket(this, 'MyBucket', {
+      bucketName: 'my-bucket-name',
+      versioned: true, // Optional: Enable versioning for the bucket
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Optional: Automatically delete the bucket when the stack is deleted
+      autoDeleteObjects: true // Optional: Automatically delete all objects in the bucket when the stack is deleted
+    });
+
+
   }
 }
