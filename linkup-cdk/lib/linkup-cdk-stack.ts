6a977d18f44768046e096b99fd223a2b14f3b432
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2024-08-20 15:53:35 +0000

    okcd ..

--- linkup-cdk/lib/linkup-cdk-stack.ts
+++ linkup-cdk/lib/linkup-cdk-stack.ts
@@ -1,13 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import { AutoScalingGroup } from 'aws-cdk-lib/aws-autoscaling';
-import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as elasticache from 'aws-cdk-lib/aws-elasticache';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import * as s3Deployment from 'aws-cdk-lib/aws-s3-deployment';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class LinkupCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -71,5 +68,28 @@ export class LinkupCdkStack extends cdk.Stack {
     writeCapacity: 1,
   });
   linkup_followers.grantFullAccess(labRole);
+
+  //lambda...
+  const index = new lambda.Function(this, 'linkup-get-index', {
+    runtime: lambda.Runtime.PYTHON_3_12,
+    code: lambda.Code.fromAsset('lambda'),
+    handler: 'index.lambda_handler',
+    role: labRole,
+  });
+
+  //linkup api gateway
+  const linkup_api_gateway = new apigateway.RestApi(this, 'linkup_api_gateway', {
+    endpointTypes: [apigateway.EndpointType.REGIONAL],
+    restApiName: 'linkup-api-gateway',
+    description: 'linkUp social Network.', 
+  });
+
+  const index_resource = linkup_api_gateway.root.addResource('index');
+  index_resource.addMethod('GET', new apigateway.LambdaIntegration(index));
+
+  new cdk.CfnOutput(this, 'API Endpoint', {
+    value: linkup_api_gateway.url,
+  });
+
   }
 }
