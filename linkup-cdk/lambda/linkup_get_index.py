0abd7d9bc92ead7c62e83ce8a260bc8995917bfb
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2024-08-20 18:52:14 +0000

    tested cdk, work!

--- linkup-cdk/lambda/linkup_get_index.py
+++ linkup-cdk/lambda/linkup_get_index.py
@@ -2,7 +2,7 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
 		'headers': {
-			'Content-Type': 'application/json'
+			'Content-Type': 'text/html'
 		},
         'body': "<!DOCTYPE html><html><body><h1>LinkUp</h1></body></html>"
     }
