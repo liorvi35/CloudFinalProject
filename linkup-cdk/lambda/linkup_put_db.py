fc1e1c8862887374827d7d4cd4f24d43d6ef5f80
liorvi35 <liorvi35@gmail.com>
2024-08-22 03:32:14 +0000

    update profile picture

--- linkup-cdk/lambda/linkup_put_db.py
+++ linkup-cdk/lambda/linkup_put_db.py
@@ -1,9 +1,16 @@
 import boto3
+import base64
+import json
 
 
 dynamodb = boto3.resource("dynamodb")
 
 users_table = dynamodb.Table("linkup-users")
+
+s3 = boto3.client("s3")
+
+profile_pictures_bucket = "linkup-profile-pictures"
+
 
 def update_attribute(account_id, attribute, new_value):
     users_table.update_item(
@@ -29,33 +36,69 @@ def lambda_handler(event, context):
                 },
                 "body": "Bad Request"
             }
-
-        count = 0
+        
+        is_picture = False
         try:
-            for attribute in ["firstName", "lastName", "hashedPassword", "email", "birthDate", "gender"]:
-                value = event.get(attribute)
-                if value:
-                    update_attribute(account_id, attribute, value)
-                    count += 1
+            base_64_file = event["base64File"]
+            format = event["format"]
+            is_picture = True
         except KeyError:
             pass
 
-        if count == 0:
+        if is_picture:
+            image_data = base64.b64decode(base_64_file)
+            new_object_key = f"{account_id}.{format}"
+
+            s3.put_object(
+                Bucket=profile_pictures_bucket,
+                Key=new_object_key,
+                Body=image_data,
+                ContentType=f"image/{format}"
+            )
+
+            profile_picture_signed = s3.generate_presigned_url(
+                "get_object",
+                Params={"Bucket": profile_pictures_bucket, "Key": new_object_key},
+                ExpiresIn=604800
+            )
+
+            update_attribute(account_id, "profilePicture", profile_picture_signed)
+
             return {
-                "statusCode": 400,
+                "statusCode": 200,
                 "headers": {
                     "Content-Type": "application/json"
                 },
-                "body": "Bad Request"
+                "body": json.dumps({ "message": "OK", "profilePicture": profile_picture_signed})
             }
+
+        else:
+            count = 0
+            try:
+                for attribute in ["firstName", "lastName", "hashedPassword", "email", "birthDate", "gender"]:
+                    value = event.get(attribute)
+                    if value:
+                        update_attribute(account_id, attribute, value)
+                        count += 1
+            except KeyError:
+                pass
+
+            if count == 0:
+                return {
+                    "statusCode": 400,
+                    "headers": {
+                        "Content-Type": "application/json"
+                    },
+                    "body": "Bad Request"
+                }
         
-        return {
-            "statusCode": 200,
-            "headers": {
-                "Content-Type": "application/json"
-            },
-            "body": "OK"
-        }
+            return {
+                "statusCode": 200,
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": "OK"
+            }
     
     except Exception as e:
         print(f"Exception: {str(e)}")
